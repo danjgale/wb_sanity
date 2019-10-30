fe6241204ec27c1c449ef294decce0ce379bf323
Dan Gale <danjgale@gmail.com>
2019-10-30 12:54:46 -0400

    added api and version

---
+++ wb_sanity/__init__.py
@@ -0,0 +1,11 @@
+from .main import (make_label_map, make_scalar_map, project_volume, 
+                   threshold_stat_map)
+
+
+__version__ = '0.0.1'
+__all__ = [
+    'make_label_map',
+    'make_scalar_map',
+    'project_volume',
+    'threshold_stat_map'
+]
